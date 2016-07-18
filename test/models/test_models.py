33e583ee1d9f3b345120bb6222aa78066a3e8667
Chris Lucas <lucasc896@gmail.com>
2016-07-18 20:08:47 +0100

    feat: continuous deployment

--- test/models/test_models.py
+++ test/models/test_models.py
@@ -1,8 +1,17 @@
 import pytest
 
-from handup.models import factories
+from handup.models import factories, models
 
 
 def test_user(session):
-    u1 = factories.UserFactory(name="Chris")
-    import pdb; pdb.set_trace()
+    factories.UserFactory(name="Chris")
+    factories.UserFactory(name="Will")
+
+    session.commit()
+
+    query = session.query(models.User)
+
+    assert query.count() == 2
+
+    for user in query.all():
+        assert user.name in ["Chris", "Will"]
