be6587ab853609da9d5034f6c336df2ca5c3ad1a
Chris Lucas <lucasc896@gmail.com>
2016-07-18 20:30:53 +0100

    test: travis with only one user

--- test/models/test_models.py
+++ test/models/test_models.py
@@ -5,13 +5,13 @@ from handup.models import factories, models
 
 def test_user(session):
     factories.UserFactory(name="Chris")
-    factories.UserFactory(name="Will")
+    # factories.UserFactory(name="Will")
 
     session.commit()
 
     query = session.query(models.User)
 
-    assert query.count() == 2
+    assert query.count() == 1
 
     for user in query.all():
         assert user.name in ["Chris", "Will"]
